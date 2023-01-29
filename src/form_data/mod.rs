50faa373538a46c6f261c612311b9d1f7faebbf9
henfur <henfur@protonmail.com>
2023-01-29 22:56:35 +0100

    move form data structures to dedicated module

---
+++ src/form_data/mod.rs
@@ -0,0 +1,54 @@
+use rocket::data::ToByteUnit;
+use rocket::form::{self, FromFormField, DataField, ValueField};
+
+use serde::{
+    Deserialize
+};
+
+pub struct AppPkg<'r> {
+    pub data: &'r [u8]
+}
+
+#[rocket::async_trait]
+impl<'r> FromFormField<'r> for AppPkg<'r> {
+    fn from_value(field: ValueField<'r>) -> form::Result<'r, Self> {
+        Ok(
+            AppPkg {data: field.value.as_bytes()}
+        )
+    }
+    
+    async fn from_data(field: DataField<'r, '_>) -> form::Result<'r, Self> {
+        let limit = field.request.limits()
+            .get("app_pkg")
+            .unwrap_or(256.mebibytes());
+
+        let bytes = field.data.open(limit).into_bytes().await?;
+        if !bytes.is_complete() {
+            Err((None, Some(limit)))?;
+        }
+
+        let bytes = bytes.into_inner();
+        let bytes = rocket::request::local_cache!(field.request, bytes);
+
+        Ok(AppPkg { data: bytes })
+    }
+}
+
+#[derive(FromForm)]
+pub struct UploadData<'r> {
+    pub policy: String,
+    pub file: AppPkg<'r>
+}
+
+#[derive(Debug, PartialEq, Eq, Deserialize)]
+pub struct DecryptionData{
+    pub id: String,
+    pub secret_key: String
+}
+
+// #[derive(FromForm, Deserialize, Serialize)]
+// pub struct UserData {
+//     username: String,
+//     api_key: String,
+//     attributes: String,
+// }
--- src/main.rs
+++ src/main.rs
@@ -18,11 +18,6 @@ use rocket::{
     form::Form,
 };
 
-use serde::{
-    Serialize,
-    Deserialize
-};
-
 mod encryption;
 use encryption::{
     encrypt_file,
@@ -31,62 +26,17 @@ use encryption::{
     decrypt_file
 };
 
-use rocket::data::ToByteUnit;
-use rocket::form::{self, FromFormField, DataField, ValueField};
+mod form_data;
+use form_data::{
+    DecryptionData,
+    UploadData
+};
 
 // File management constants
 const UPLOAD_PATH: &'static str = "./upload/";
 // PKG Parameters
 const PKG_MASTER_DIR_PATH: &'static str = "./pkg/";
 
-struct AppPkg<'r> {
-    data: &'r [u8]
-}
-
-#[rocket::async_trait]
-impl<'r> FromFormField<'r> for AppPkg<'r> {
-    fn from_value(field: ValueField<'r>) -> form::Result<'r, Self> {
-        Ok(
-            AppPkg {data: field.value.as_bytes()}
-        )
-    }
-    
-    async fn from_data(field: DataField<'r, '_>) -> form::Result<'r, Self> {
-        let limit = field.request.limits()
-            .get("app_pkg")
-            .unwrap_or(256.mebibytes());
-
-        let bytes = field.data.open(limit).into_bytes().await?;
-        if !bytes.is_complete() {
-            println!("Test");
-            Err((None, Some(limit)))?;
-        }
-
-        let bytes = bytes.into_inner();
-        let bytes = rocket::request::local_cache!(field.request, bytes);
-
-        Ok(AppPkg { data: bytes })
-    }
-}
-
-#[derive(FromForm)]
-struct UploadData<'r> {
-    policy: String,
-    file: AppPkg<'r>
-}
-
-#[derive(FromForm, Deserialize, Serialize)]
-struct UserData {
-    username: String,
-    api_key: String,
-    attributes: String,
-}
-
-#[derive(Debug, PartialEq, Eq, Deserialize)]
-struct DecryptionData{
-    id: String,
-    secret_key: String
-}
 
 // Initializes PKG keys if they don't exist
 // Starts the webserver
