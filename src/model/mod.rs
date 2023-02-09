afbd9e937810aee6efb08a819766f1a6bfa91649
henfur <henfur@protonmail.com>
2023-02-09 01:07:59 +0100

    add ciphertext file table

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -7,11 +7,12 @@ use serde::{
 };
 
 use crate::schema::users;
+use crate::schema::ciphertext_files;
 
 pub fn establish_connection() -> MysqlConnection {
     dotenv().ok();
 
-    let database_url = env::var("DATABASE_HOST").expect("DATABASE_HOST must be set");
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     MysqlConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
 }
@@ -32,12 +33,29 @@ pub struct NewUser<'a> {
     pub email: &'a str
 }
 
-pub fn create_user(conn: &mut MysqlConnection, username: &str, email: &str) -> User {
+#[derive(Queryable, Insertable, Debug, PartialEq, Eq, Deserialize)]
+#[diesel(table_name = ciphertext_files)]
+pub struct CiphertextFile<'a> {
+    pub original_file_name: &'a str,
+    pub ciphertext_id: &'a str
+}
+
+pub fn create_user(conn: &mut MysqlConnection, username: &str, email: &str) -> () {
 
     let new_user = NewUser { username, email };
 
     diesel::insert_into(users::table)
         .values(&new_user)
-        .get_result(conn)
-        .expect("Error saving new user")
+        .execute(conn)
+        .unwrap();
+}
+
+pub fn create_ciphertext_file(conn: &mut MysqlConnection, original_file_name: &str, ciphertext_id: &str) -> () {
+
+    let new_ciphertext_file = CiphertextFile{ original_file_name, ciphertext_id };
+
+    diesel::insert_into(ciphertext_files::table)
+        .values(&new_ciphertext_file)
+        .execute(conn)
+        .unwrap();
 }
--- src/schema.rs
+++ src/schema.rs
@@ -1,6 +1,13 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    ciphertext_files (original_file_name, ciphertext_id) {
+        original_file_name -> Varchar,
+        ciphertext_id -> Varchar,
+    }
+}
+
+diesel::table! {
     users (id) {
         id -> Unsigned<Bigint>,
         username -> Varchar,
@@ -9,3 +16,8 @@ diesel::table! {
         department -> Nullable<Varchar>,
     }
 }
+
+diesel::allow_tables_to_appear_in_same_query!(
+    ciphertext_files,
+    users,
+);
